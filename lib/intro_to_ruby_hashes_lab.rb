bec59d230ee0831e4ea6733f7ba0718a3608aed4
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-31 00:35:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,11 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  { 
+    example_key: "example value"
+  }
 end
 
 def pioneer
